2d2f840f6bc24b76872907846bbe12e7ddd40675
Jazzlyn <jazzlyn406@gmail.com>
2021-12-01 11:34:04 -0700

    hideous way of doing this

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,36 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var increaseCount = 0
+        var previousNumber = 0
+        for ((index, value) in input.withIndex()) {
+            if (index == 0) {
+                previousNumber = value.toInt()
+            } else {
+                if (previousNumber < value.toInt()) {
+                    increaseCount ++
+                }
+                previousNumber = value.toInt()
+            }
+        }
+        return increaseCount
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var increaseCount = 0
+        var current = 0
+        var next = 0
+        for ((index, value) in input.withIndex()) {
+            current = input[index].toInt() + input[index + 1].toInt() + input[index + 2].toInt()
+            next = input[index + 1].toInt() + input[index + 2].toInt() + input[index + 3].toInt()
+            println("current: " + current)
+            println("next: " + next)
+            if (current < next) {
+                increaseCount ++
+                println(increaseCount)
+            }
+        }
+        return increaseCount
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
